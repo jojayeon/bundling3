d2957b158a2000097c9c5ddfb9c60d3ba5b033b5
jojayeon <jojayeon6152@gmail.com>
2024-07-12 19:51:09 +0900

    ulli import전

---
+++ src/module/ulli.tsx
@@ -0,0 +1,18 @@
+// ulil 틀
+
+import React from "react"
+
+interface ulli {
+  litext: string;
+}
+
+const Ulli:React.FC<ulli> = ({litext}) =>{
+  return(
+    <ul>
+      <li>{litext[0]}</li>
+      <li>{litext[1]}</li>
+    </ul>
+  )
+}
+
+export default Ulli;
