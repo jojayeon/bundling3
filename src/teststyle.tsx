de1d5ec6488dfd83266a2625c40244fa4c629a45
jojayeon <jojayeon6152@gmail.com>
2024-07-12 18:24:05 +0900

    스타일 따로 뺀 파일

---
+++ src/teststyle.tsx
@@ -0,0 +1,12 @@
+
+import React from "react"
+
+const Style1: React.CSSProperties = {
+  width : "100px",
+  height : "100px",
+  backgroundColor: "black",
+  margin : "10%",
+  color : "#fff",
+}
+
+export default Style1
