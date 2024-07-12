4b2ef3ec3d165b17ac0d7cdb6ef75982e50dd2be
jojayeon <jojayeon6152@gmail.com>
2024-07-12 09:53:04 +0900

    연습중인 html 변환중인 파일

---
+++ src/test.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import A from "./title"
+import title123 from "../static/testtitle"
+
+const Test:React.FC = () => {
+  return (
+    <div>
+      <A divtext= {title123}/>
+      <div>title2</div>
+      <div>
+        <div>text</div>
+        <ul>
+          <li>text1</li>
+          <li>text2</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Test
+
+
+//1.  여기서 title, title2, text 같게 만들어서 글만 가져오는 것에 글을 다르게만 해주면 되지않을까?
+//2.  아래 부분을 따로만들어서 
+{/* <ul>
+  <li>text1</li>
+  <li>text2</li>
+</ul> */}
+
+{/* <div>
+  <title(함수) 매개변수= {글 있는 변수} />
+  <title(함수) 매개변수= {글 있는 변수} />
+  <div>
+    <title(함수) 매개변수= {글 있는 변수} />
+    <luli 매개변수 = {글 모은 배열 변수} />
+  </div>
+</div> */}
