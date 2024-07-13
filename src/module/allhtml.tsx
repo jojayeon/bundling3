756a612468d819e54d649f2bb9d1d8082661c065
jojayeon <jojayeon6152@gmail.com>
2024-07-13 18:07:17 +0900

    text파일 import

--- src/module/allhtml.tsx
+++ src/module/allhtml.tsx
@@ -4,27 +4,34 @@
 
 import React from "react"
 import Divhtml from "./div";
+import Ulli from "./ulli"
+
+
+// text
+import maintitle1 from "../../static/miantitle1"
+import maintitle2 from "../../static/miantitle2"
+import ultext from "../../static/ultext"
+import text1 from "../../static/text1"
+import text2 from "../../static/text2"
+import litext1 from "../../static/litext1-1"
+import litext2 from "../../static/litext2-1"
+
+
 
 const ALLhtml:React.FC = () =>{
   return(
     <div>
-      <Divhtml anytext = "aa"/>
-      <Divhtml anytext = "aa"/>
+      <Divhtml anytext = {maintitle1}/>
+      <Divhtml anytext = {text1}/>
       <div>
-        <Divhtml anytext = "aa"/>
-        <ul>
-          <li>litext1-1</li>
-          <li>litext1-2</li>
-        </ul>
+        <Divhtml anytext = {ultext}/>
+        <Ulli litext = {litext1}/>
       </div>
-      <Divhtml anytext = "aa"/>
-      <Divhtml anytext = "aa"/>
-      <div>
-        <Divhtml anytext = "aa"/>
-        <ul>
-          <li>litext2-1</li>
-          <li>litext2-2</li>
-        </ul>
+      <Divhtml anytext =  {maintitle2}/>
+      <Divhtml anytext = {text2}/>
+      <div id="aa">
+        <Divhtml anytext = {ultext}/>
+        <Ulli litext = {litext2}/>
       </div>
     </div>
   )
