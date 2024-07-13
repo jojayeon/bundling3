8114436198f8a6b93e059e0edb96f6980584d45e
jojayeon <jojayeon6152@gmail.com>
2024-07-13 15:56:34 +0900

    render 오타, dom 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 //dom 추가 , allhtml 받아서 reander할 파일
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ALLhtml from "./module/allhtml";
 
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(<ALLhtml />)
 //todo dom 조작 작성 
