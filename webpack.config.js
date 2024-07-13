4912942b9ec4224076199c77c3f7e3db41b8d0e3
jojayeon <jojayeon6152@gmail.com>
2024-07-13 17:30:19 +0900

    경로 오류 -> 수정

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 
 module.exports = {
-  entry:"./index.tsx",
+  entry:"./src/index.tsx",
   output:{
     filename: "index.bundle.js",
     path : path.resolve(__dirname, "dist")
